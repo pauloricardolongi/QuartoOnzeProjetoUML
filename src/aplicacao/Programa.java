10041d2cfcf90e6b02651045536588d6d8a2d6b1
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-17 02:14:21 -0300

    Peça Posição

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import boardgame.Position;
+
+public class Programa {
+
+	public static void main(String[] args) {
+	Position pos = new Position(3, 5);
+	System.out.println(pos);
+
+	}
+
+}
