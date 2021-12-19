eb2921b25746ccc2e26c8a7403bc5b29d2fa7736
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-19 01:30:29 -0300

    classe BoardException

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,7 +26,7 @@ public class ChessMatch {
 	}//iniciar a partida, colocando as peças no tabuleiro
 	private void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
 		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
 }
